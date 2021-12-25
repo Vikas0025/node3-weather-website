4afc19633a9182718afaf1805f2f34a669485e9c
pratheek77 <pratheekanukonty@gmail.com>
2021-12-25 17:17:33 +0530

    Remove unnecessary console.log call

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,3 @@
-console.log('working')
-fetch('http://localhost:3000/weather?address=!').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }
-        else console.log(data)
-    })
-})
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
